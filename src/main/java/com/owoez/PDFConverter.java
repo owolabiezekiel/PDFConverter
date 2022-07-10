75baea2af1f27a8f9c717023b8a7198702e4b93e
tobilobaowolabi <tobiloba.owolabi@stitch.money>
2022-07-10 22:03:12 +0100

    Code cleanup. Handle space in filename from breaking conversion

--- src/main/java/com/owoez/PDFConverter.java
+++ src/main/java/com/owoez/PDFConverter.java
@@ -1,5 +1,6 @@
 package com.owoez;
 
+import com.aspose.pdf.DocSaveOptions;
 import com.aspose.pdf.Document;
 import com.aspose.pdf.SaveFormat;
 
@@ -16,16 +17,26 @@ public class PDFConverter {
 	com.aspose.pdf.LocaleOptions.setLocale(new Locale("en", "US"));
 	String destinationFilePath;
 
+
 	//Check if user supplied the input file location as an argument or not
 	if (args.length <= 0) {
 	  System.out.println("You need to pass the file location as an argument");
 	  return;
 	}
 
+	String filePathString = args[0];
+
+
+	//Check that the filepath doesn't contain special characters like space
+	System.out.println("Checking input file path....");
+	if (!isValidPdfFilePath(filePathString)){
+	  System.out.println("Invalid file path. Please make sure to escape all special characters in file path");
+	  return;
+	}
+
 
 	//Load the source file
 	System.out.println("Loading input file....");
-	String filePathString = args[0];
 	Document document = new Document(filePathString);
 	System.out.println("File load complete.....");
 
@@ -36,12 +47,7 @@ public class PDFConverter {
 	System.out.println("Determining output file destination");
 	if (args.length <= 1) {
 	  System.out.println("User didnt set output file destination...");
-	  String[] splitFilePathString = filePathString.split("/");
-	  String fileNameWithExtension = splitFilePathString[splitFilePathString.length - 1];
-	  String[] splitFileNameAndExtension = fileNameWithExtension.split("\\.");
-	  String fileName = splitFileNameAndExtension[0];
-	  String userHomeDir = System.getProperty("user.home");
-	  destinationFilePath = userHomeDir + "/Desktop/" + fileName + ".docx";
+	  destinationFilePath = getOutputFileName(filePathString);
 	  System.out.println("Proceeding to save file on users desktop..." + destinationFilePath);
 	} else {
 	  destinationFilePath = args[1];
@@ -53,5 +59,23 @@ public class PDFConverter {
 	System.out.println("Converting PDF and writing output......");
 	document.save(destinationFilePath, SaveFormat.DocX);
 	System.out.println("Conversion done!");
+  }
+
+  static boolean isValidPdfFilePath(String filePathString) {
+	String[] splitFilePathString = filePathString.split("/");
+	System.out.println("Filename is: " + splitFilePathString[splitFilePathString.length -1]);
+	String fileNameWithExtension = splitFilePathString[splitFilePathString.length - 1];
+	String[] splitFileNameAndExtension = fileNameWithExtension.split("\\.");
+	String fileExtension = splitFileNameAndExtension[splitFileNameAndExtension.length - 1];
+	return fileExtension.equalsIgnoreCase("pdf");
+  }
+
+  static String getOutputFileName(String filePathString){
+	String[] splitFilePathString = filePathString.split("/");
+	String fileNameWithExtension = splitFilePathString[splitFilePathString.length - 1];
+	String[] splitFileNameAndExtension = fileNameWithExtension.split("\\.");
+	String fileName = splitFileNameAndExtension[0];
+	String userHomeDir = System.getProperty("user.home");
+	return userHomeDir + "/Desktop/" + fileName + ".docx";
   }
 }
